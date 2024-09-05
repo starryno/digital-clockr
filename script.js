8ae47da55f6abb57c55e4bfa5be34a94cbb53178
starryno <rayyansmkpi@gmail.com>
2024-09-05 14:58:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const timeElement = document.getElementById('time');
   const now = new Date();
   const hours = String(now.getHours()).padStart(2, '0');
   const minutes = String(now.getMinutes()).padStart(2, '0');
-  const second = String(now.getSecond()).padStart(2, '0');
-  timeElement.textContent = '${hours}:$(minutes):${seconds}';
+  const second = String(now.getSeconds()).padStart(2, '0');
+  timeElement.textContent = '${hours}:${minutes}:${seconds}';
   
 }
 
