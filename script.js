455a33f1a9db51ffe1da9c99b2471ccfe9dba8d3
starryno <rayyansmkpi@gmail.com>
2024-09-05 14:42:01 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function updateTime() {
+  const timeElement = document.getElementById('time');
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const second = Sting(now.getSecond()).padStart(2, '0');
+  timeElement.textContent = '${hours}:${seconds}';
+}
+
+setInterval(updateTime, 1000);
+updateTime(); // Initial call to display time immediately
